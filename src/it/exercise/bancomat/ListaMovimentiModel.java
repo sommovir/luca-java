29a51cae16933639d15988d92025b621e138412c
User <User@User-PC>
2018-09-29 17:10:50 +0200

    preparazione esercizio

---
+++ src/it/exercise/bancomat/ListaMovimentiModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.exercise.bancomat;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Luca Coraci <luca.coraci@istc.cnr.it>
+ */
+public class ListaMovimentiModel extends DefaultListModel<AbstractBankOperation> {
+    
+}
